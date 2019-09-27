51d981b8ab4c0884aaf45a324e88e9ed854b487c
Matthew Nelson <05nelsonm@protonmail.com>
2019-09-27 13:35:27 -0400

    All of the things

--- app/src/main/java/com/lambdaschool/choretracker/viewmodel/ParentChoreDetailActivityViewModel.kt
+++ app/src/main/java/com/lambdaschool/choretracker/viewmodel/ParentChoreDetailActivityViewModel.kt
@@ -3,10 +3,16 @@ package com.lambdaschool.choretracker.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.lambdaschool.choretracker.model.Child
+import com.lambdaschool.choretracker.model.ChildLoginCredential
+import com.lambdaschool.choretracker.util.repo
 
 class ParentChoreDetailActivityViewModel: ViewModel() {
 
-    fun getAllChildForParentId(parentId: Int): LiveData<List<Child>> {
-        return getAllChildForParentId(parentId)
-    }
+    fun getAllChildForParentId(parentId: Int): LiveData<List<Child>> =
+        repo.getAllChildForParentId(parentId)
+
+    fun createChild(child: Child) = repo.createChild(child)
+
+    fun createChildLoginCredential(childCreds: ChildLoginCredential) =
+        repo.createChildLoginCredential(childCreds)
 }
