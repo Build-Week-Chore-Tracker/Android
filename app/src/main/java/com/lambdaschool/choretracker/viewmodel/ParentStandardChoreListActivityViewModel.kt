6024f8804ae5d283b7fa88923cff7aa14b0e8ed1
Matthew Nelson <05nelsonm@protonmail.com>
2019-09-27 00:16:55 -0400

    added repo call

--- app/src/main/java/com/lambdaschool/choretracker/viewmodel/ParentStandardChoreListActivityViewModel.kt
+++ app/src/main/java/com/lambdaschool/choretracker/viewmodel/ParentStandardChoreListActivityViewModel.kt
@@ -1,9 +1,12 @@
 package com.lambdaschool.choretracker.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.lambdaschool.choretracker.model.Chore
 import com.lambdaschool.choretracker.util.repo
 
 class ParentStandardChoreListActivityViewModel : ViewModel() {
 
-    fun getAllChores(parentId: Int) = repo.getAllChoresForParentId(parentId)
+    fun getAllChoresForParentId(parentId: Int) = repo.getAllChoresForParentId(parentId)
+
+    fun createChore(chore: Chore) = repo.createChore(chore)
 }
